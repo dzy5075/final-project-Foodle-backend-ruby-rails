56a70daca9e64aa11ae1ae48b354ad2cd119f48a
Daniel Yim <daniel.yim96@gmail.com>
2023-02-24 15:32:09 -0500

    creating serializers

--- app/controllers/favorite_recipes_controller.rb
+++ app/controllers/favorite_recipes_controller.rb
@@ -1,2 +1,14 @@
 class FavoriteRecipesController < ApplicationController
+    wrap_parameters format: []
+    skip_before_action :authorized, only: :index
+
+    def index 
+        favs = FavoriteRecipe.all
+        render json: favs
+    end
+
+    def create 
+        newfav = FavoriteRecipe.create!(recipe_params)
+        render json: newfav, status: :created
+    end
 end
--- app/controllers/food_with_nutrients_controller.rb
+++ app/controllers/food_with_nutrients_controller.rb
@@ -1,5 +1,5 @@
 class FoodWithNutrientsController < ApplicationController
-
+    skip_before_action :authorized, only: :show
     def show 
         food = FoodWithNutrient.find(params[:id])
         render json: food, status: :ok
