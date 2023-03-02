bb468f10331c19c5320641267463a7e2d8ad21d9
Daniel Yim <daniel.yim96@gmail.com>
2023-03-02 17:18:37 -0500

    added favorite params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
-    # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     before_action :authorized
 
 
