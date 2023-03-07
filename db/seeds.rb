eb914241b278f7476ccea677c7de56b9c2e3ad95
Daniel Yim <daniel.yim96@gmail.com>
2023-03-06 23:09:57 -0500

    revised seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,46 +23,6 @@ Recipe.create([
           cuisine: "British",
           instruction: "Preheat the oven to 200C/180C Fan/Gas 6.\nPut the biscuits in a large re-sealable freezer bag and bash with a rolling pin into fine crumbs. Melt the butter in a small pan, then add the biscuit crumbs and stir until coated with butter. Tip into the tart tin and, using the back of a spoon, press over the base and sides of the tin to give an even layer. Chill in the fridge while you make the filling.\nCream together the butter and sugar until light and fluffy. You can do this in a food processor if you have one. Process for 2-3 minutes. Mix in the eggs, then add the ground almonds and almond extract and blend until well combined.\nPeel the apples, and cut thin slices of apple. Do this at the last minute to prevent the apple going brown. Arrange the slices over the biscuit base. Spread the frangipane filling evenly on top. Level the surface and sprinkle with the flaked almonds.\nBake for 20-25 minutes until golden-brown and set.\nRemove from the oven and leave to cool for 15 minutes. Remove the sides of the tin. An easy way to do this is to stand the tin on a can of beans and push down gently on the edges of the tin.\nTransfer the tart, with the tin base attached, to a serving plate. Serve warm with cream, crème fraiche or ice cream.",
           image: "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
-          video: "https://www.youtube.com/watch?v=rp8Slv4INLk"
-        },
-        {
-          name: "Apple & Blackberry Crumble",
-          category: "Dessert",
-          cuisine: "British",
-          instruction: "Heat oven to 190C/170C fan/gas 5. Tip the flour and sugar into a large bowl. Add the butter, then rub into the flour using your fingertips to make a light breadcrumb texture. Do not overwork it or the crumble will become heavy. Sprinkle the mixture evenly over a baking sheet and bake for 15 mins or until lightly coloured.\nMeanwhile, for the compote, peel, core and cut the apples into 2cm dice. Put the butter and sugar in a medium saucepan and melt together over a medium heat. Cook for 3 mins until the mixture turns to a light caramel. Stir in the apples and cook for 3 mins. Add the blackberries and cinnamon, and cook for 3 mins more. Cover, remove from the heat, then leave for 2-3 mins to continue cooking in the warmth of the pan.\nTo serve, spoon the warm fruit into an ovenproof gratin dish, top with the crumble mix, then reheat in the oven for 5-10 mins. Serve with vanilla ice cream.",
-          image: "https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg",
-          video: "https://www.youtube.com/watch?v=4vhcOwVBDO4"
-        },
-        {
-          name: "Apam balik",
-          category: "Dessert",
-          cuisine: "Malaysian",
-          instruction: "Mix milk, oil and egg together. Sift flour, baking powder and salt into the mixture. Stir well until all ingredients are combined evenly.\n\nSpread some batter onto the pan. Spread a thin layer of batter to the side of the pan. Cover the pan for 30-60 seconds until small air bubbles appear.\n\nAdd butter, cream corn, crushed peanuts and sugar onto the pancake. Fold the pancake into half once the bottom surface is browned.\n\nCut into wedges and best eaten when it is warm.",
-          image: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg",
-          video: "https://www.youtube.com/watch?v=6R8ffRRJcrg"
-        },
-        {
-          name: "Ayam Percik",
-          category: "Chicken",
-          cuisine: "Malaysian",
-          instruction: "In a blender, add the ingredients for the spice paste and blend until smooth.\nOver medium heat, pour the spice paste in a skillet or pan and fry for 10 minutes until fragrant. Add water or oil 1 tablespoon at a time if the paste becomes too dry. Don't burn the paste. Lower the fire slightly if needed.\nAdd the cloves, cardamom, tamarind pulp, coconut milk, water, sugar and salt. Turn the heat up and bring the mixture to boil. Turn the heat to medium low and simmer for 10 minutes. Stir occasionally. It will reduce slightly. This is the marinade/sauce, so taste and adjust seasoning if necessary. Don't worry if it's slightly bitter. It will go away when roasting.\nWhen the marinade/sauce has cooled, pour everything over the chicken and marinate overnight to two days.\nPreheat the oven to 425 F.\nRemove the chicken from the marinade. Spoon the marinade onto a greased (or aluminum lined) baking sheet. Lay the chicken on top of the sauce (make sure the chicken covers the sauce and the sauce isn't exposed or it'll burn) and spread the remaining marinade on the chicken. Roast for 35-45 minutes or until internal temp of the thickest part of chicken is at least 175 F.\nLet chicken rest for 5 minutes. Brush the chicken with some of the oil. Serve chicken with the sauce over steamed rice (or coconut rice).",
-          image: "https://www.themealdb.com/images/media/meals/020z181619788503.jpg",
-          video: "https://www.youtube.com/watch?v=9ytR28QK6I8"
-        },
-        {
-          name: "Bakewell tart",
-          category: "Dessert",
-          cuisine: "British",
-          instruction: "To make the pastry, measure the flour into a bowl and rub in the butter with your fingertips until the mixture resembles fine breadcrumbs. Add the water, mixing to form a soft dough.\nRoll out the dough on a lightly floured work surface and use to line a 20cm/8in flan tin. Leave in the fridge to chill for 30 minutes.\nPreheat the oven to 200C/400F/Gas 6 (180C fan).\nLine the pastry case with foil and fill with baking beans. Bake blind for about 15 minutes, then remove the beans and foil and cook for a further five minutes to dry out the base.\nFor the filing, spread the base of the flan generously with raspberry jam.\nMelt the butter in a pan, take off the heat and then stir in the sugar. Add ground almonds, egg and almond extract. Pour into the flan tin and sprinkle over the flaked almonds.\nBake for about 35 minutes. If the almonds seem to be browning too quickly, cover the tart loosely with foil to prevent them burning.",
-          image: "https://www.themealdb.com/images/media/meals/wyrqqq1468233628.jpg",
-          video: "https://www.youtube.com/watch?v=1ahpSTf_Pvk",
-        },
-        {
-          name: "Apple Frangipan Tart",
-          category: "Dessert",
-          cuisine: "British",
-          instruction: "Preheat the oven to 200C/180C Fan/Gas 6.\nPut the biscuits in a large re-sealable freezer bag and bash with a rolling pin into fine crumbs. Melt the butter in a small pan, then add the biscuit crumbs and stir until coated with butter. Tip into the tart tin and, using the back of a spoon, press over the base and sides of the tin to give an even layer. Chill in the fridge while you make the filling.\nCream together the butter and sugar until light and fluffy. You can do this in a food processor if you have one. Process for 2-3 minutes. Mix in the eggs, then add the ground almonds and almond extract and blend until well combined.\nPeel the apples, and cut thin slices of apple. Do this at the last minute to prevent the apple going brown. Arrange the slices over the biscuit base. Spread the frangipane filling evenly on top. Level the surface and sprinkle with the flaked almonds.\nBake for 20-25 minutes until golden-brown and set.\nRemove from the oven and leave to cool for 15 minutes. Remove the sides of the tin. An easy way to do this is to stand the tin on a can of beans and push down gently on the edges of the tin.\nTransfer the tart, with the tin base attached, to a serving plate. Serve warm with cream, crème fraiche or ice cream.",
-          image: "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
           video: "https://www.youtube.com/watch?v=rp8Slv4INLk",
           
           },
